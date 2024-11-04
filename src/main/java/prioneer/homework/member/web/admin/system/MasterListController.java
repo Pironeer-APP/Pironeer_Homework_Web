6b8d6d125a78c7a929c6e7451c375020f42fe9c3
devkev00 <kimminsoo116@gmail.com>
2024-11-04 19:42:43 +0900

    feat: SystemJoinController, UserNewController, MasterListController 구현, Member 엔티티 필드 타입 변경

--- src/main/java/prioneer/homework/member/web/admin/system/MasterListController.java
+++ src/main/java/prioneer/homework/member/web/admin/system/MasterListController.java
@@ -1,4 +1,74 @@
 package prioneer.homework.member.web.admin.system;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import prioneer.homework.member.domain.Member;
+import prioneer.homework.member.repository.MemberRepository;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class MasterListController {
+    // 마스터 관리자는 들어온 관리자 회원가입 리스트에서 셀렉트 박스를 통해 admin 권한을 부여한다
+    // 처음 회원가입한 관리자들은 모두 preadmin이다
+    // url은 /system/master/list
+
+    private final MemberRepository memberRepository;
+
+    // 관리자 신청 확인 명단 페이지를 보여줌
+    @GetMapping("system/master/list")
+    public String masterList(Model model) {
+        List<Member> preAdminList = memberRepository.findByRole("preadmin");
+        // Enum이 아니라서 일단 이렇게 함,,, 나중에 수정하나?
+
+        model.addAttribute("preAdminList", preAdminList);
+        return "system/master/list";
+    }
+
+    // 관리자 권한 부여
+    @PostMapping("system/master/updateToAdmin")
+    @ResponseBody
+    public ResponseEntity<String> updateToAdmin(
+            @RequestParam("phone") String phone,
+            @RequestParam("role") String role) {
+        try {
+
+            Member member = memberRepository.findByPhone(phone)
+                    .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다"));
+
+            member.setRole(role);
+            memberRepository.save(member);
+
+            return ResponseEntity.ok("권한 변경 성공");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("권한 변경 중 오류 발생");
+        }
+    }
+
+    // preadmin 삭제
+    @PostMapping("system/master/deletePreadmin")
+    @ResponseBody
+    public ResponseEntity<String> deletePreadmin(
+            @RequestParam("phone") String phone) {
+        try {
+            if (!memberRepository.existsByPhone(phone)) {
+                return ResponseEntity.badRequest().body("사용자를 찾을 수 없습니다");
+            }
+
+            memberRepository.deleteByPhone(phone);
+            return ResponseEntity.ok("회원 삭제 성공");
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("회원 삭제 중 오류 발생");
+        }
+    }
+
+
 }
--- src/main/java/prioneer/homework/member/web/admin/system/SystemListController.java
+++ src/main/java/prioneer/homework/member/web/admin/system/SystemListController.java
@@ -1,4 +1,48 @@
 package prioneer.homework.member.web.admin.system;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import prioneer.homework.member.domain.Member;
+import prioneer.homework.member.repository.MemberRepository;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class SystemListController {
+    // 일반 관리자가 보는 명단 화면
+    // 부원들의 이름, 보증금 현황, 방어권 개수를 볼 수 있고 회원삭제 가능
+    // url은 /system/list
+
+    private final MemberRepository memberRepository;
+
+    // 명단 페이지
+    @GetMapping("/system/list")
+    public String systemList(Model model) {
+        List<Member> memberList = memberRepository.findByRole("member");
+        model.addAttribute("memberList", memberList);
+        return "system/list";
+    }
+
+    // 회원 삭제
+    @PostMapping("/system/deleteMember")
+    @ResponseBody
+    public ResponseEntity<String> deleteMember(@RequestParam("phone") String phone) {
+        try {
+            if (!memberRepository.existsByPhone(phone)) {
+                return ResponseEntity.badRequest().body("존재하지 않는 회원입니다.");
+            }
+
+            memberRepository.deleteByPhone(phone);
+            return ResponseEntity.ok().body("회원 삭제 성공");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("회원 삭제 중 오류 발생");
+        }
+    }
 }
