09d81e5eb8885178d1cc95bef2427f5fc857a3e1
chltmdgh522 <chltmdgh517@naver.com>
2024-11-05 15:53:24 +0900

    feat: 홈 컨트롤러 로직 만듬 로그인 됐을때 안됐을때

--- src/main/java/prioneer/homework/home/web/HomeController.java
+++ src/main/java/prioneer/homework/home/web/HomeController.java
@@ -1,4 +1,33 @@
 package prioneer.homework.home.web;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import prioneer.homework.config.session.SessionConst;
+import prioneer.homework.member.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model,
+                       @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
+                       Member loginMember) {
+
+
+        if (loginMember == null) {
+            return "home/noLoginHome";
+        }
+
+        return "home/loginHome";
+    }
+
 }
