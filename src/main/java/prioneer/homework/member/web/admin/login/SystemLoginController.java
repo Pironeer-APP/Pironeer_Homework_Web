e5cecde4d4450d582440c6dd9e6dc9e1aa6d01ed
chltmdgh522 <chltmdgh517@naver.com>
2024-11-20 01:17:02 +0900

    fix: 관리자 로그인 회원가입 일부 로직 수정

--- src/main/java/prioneer/homework/member/web/admin/login/SystemLoginController.java
+++ src/main/java/prioneer/homework/member/web/admin/login/SystemLoginController.java
@@ -1,14 +1,22 @@
 package prioneer.homework.member.web.admin.login;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import prioneer.homework.config.session.SessionConst;
 import prioneer.homework.member.domain.Member;
 import prioneer.homework.member.service.admin.AdminMemberService;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,24 +25,49 @@ public class SystemLoginController {
 
     private final AdminMemberService adminMemberService;
 
-    @GetMapping("/system")
-    public String getSystemLogin(Model model,
-                                 @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
-                                 Member loginMember) {
 
-        try {
-            if (loginMember == null) {
-                return "로그인 안 된 홈 html";
-            }
+    @GetMapping("/system/login")
+    public String getSystemLogin(@ModelAttribute("member") Member member
+            , Model model) {
 
-        // 로그인 회원 정보 비교
-        Member member = adminMemberService.getLoginMember(loginMember);
+
         model.addAttribute("member", member);
+        return "admin/admin_login";
 
-        return "로그인 된 홈 html";
 
-        } catch (IllegalStateException e) {
-            return "admin/admin_home_loginbefore";
+    }
+
+    @PostMapping("/system/login")
+    public String postSystemLogin(@Validated @ModelAttribute Member member, BindingResult bindingResult,
+                                  HttpServletRequest request) {
+        Optional<Member> findPhoneMember = adminMemberService.getLoginMember(member);
+        if (findPhoneMember.isEmpty()) {
+            bindingResult.reject("loginFail", "전화번호가 일치하지 않습니다");
+            return "admin/admin_login";
         }
+
+        Optional<Member> findNameMember = adminMemberService.getNameMember(member);
+        if (findNameMember.isEmpty()) {
+            bindingResult.reject("loginFail", "이름이 일치하지 않습니다");
+            return "admin/admin_login";
+        }
+
+        Member passwordMember = adminMemberService.passwordCheck(member);
+        if (passwordMember == null) {
+            bindingResult.reject("loginFail", "비밀번호가 일치하지 않습니다");
+            return "admin/admin_login";
+        }
+
+        if (passwordMember.getRole().equals("PREADMIN")) {
+            bindingResult.reject("loginFail", "교육팀장님께 문의주세요");
+            return "admin/admin_login";
+        }
+
+        //로그인 성공
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, passwordMember);
+
+        return "redirect:/system";
+
     }
 }
