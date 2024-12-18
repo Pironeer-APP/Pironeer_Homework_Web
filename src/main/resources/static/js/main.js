455956e03d0f7d721531e948859c0ca1b3ae28bb
chltmdgh522 <chltmdgh517@naver.com>
2024-12-18 17:07:24 +0900

    fix: 마우스 커서 수정3 모바일 환경 고려

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,26 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const cursor = document.createElement('div');
-    cursor.classList.add('cursor');
-    document.body.appendChild(cursor);
+    const isMobile = window.matchMedia('(max-width: 768px)').matches;
 
-    const updateCursorPosition = (e) => {
-        cursor.style.left = `${e.clientX}px`;
-        cursor.style.top = `${e.clientY}px`;
-    };
+    if (!isMobile) {
+        const cursor = document.createElement('div');
+        cursor.classList.add('cursor');
+        document.body.appendChild(cursor);
 
-    document.addEventListener('mousemove', updateCursorPosition);
+        const updateCursorPosition = (e) => {
+            cursor.style.left = `${e.clientX}px`;
+            cursor.style.top = `${e.clientY}px`;
+        };
 
-    // 화면 밖으로 나갈 때 동그라미 숨기기
-    document.addEventListener('mouseleave', () => {
-        cursor.style.opacity = '0';
-    });
+        document.addEventListener('mousemove', updateCursorPosition);
 
-    document.addEventListener('mouseenter', () => {
-        cursor.style.opacity = '1';
-    });
+        // 화면 밖으로 나갈 때 동그라미 숨기기
+        document.addEventListener('mouseleave', () => {
+            cursor.style.opacity = '0';
+        });
 
-    // 커스텀 커서를 유지하고 기본 커서를 완전히 제거
-    document.querySelectorAll('input, textarea, button, select, a, label, div, p,h1, h2, h3,h4,h5,h6, span').forEach((el) => {
-        el.style.cursor = 'none'; // 기본 커서를 제거
-    });
+        document.addEventListener('mouseenter', () => {
+            cursor.style.opacity = '1';
+        });
+
+        // 커스텀 커서를 유지하고 기본 커서를 완전히 제거
+        document.querySelectorAll('input, textarea, button, select, a, label, div, p,h1, h2, h3,h4,h5,h6, span').forEach((el) => {
+            el.style.cursor = 'none'; // 기본 커서를 제거
+        });
+    } else {
+        console.log('모바일 환경에서는 커스텀 커서를 비활성화합니다.');
+    }
 });
